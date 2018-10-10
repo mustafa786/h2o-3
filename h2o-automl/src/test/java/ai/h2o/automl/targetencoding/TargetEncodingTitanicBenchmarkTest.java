031f052f209c0f81fd7bf697744b5d0d785b3434
Andrey Spiridonov <andrey@h2o.ai>
2018-10-09 18:27:31 -0700

    PUBDEV-5378: Added immutability test for TargetEncoder. Removed unnecessary copying of frame in TargetEncoder.

--- h2o-automl/src/test/java/ai/h2o/automl/targetencoding/TargetEncodingTitanicBenchmarkTest.java
+++ h2o-automl/src/test/java/ai/h2o/automl/targetencoding/TargetEncodingTitanicBenchmarkTest.java
@@ -8,7 +8,6 @@ import hex.genmodel.utils.DistributionFamily;
 import hex.tree.gbm.GBM;
 import hex.tree.gbm.GBMModel;
 import org.junit.*;
-import water.H2O;
 import water.Key;
 import water.Scope;
 import water.fvec.Frame;
